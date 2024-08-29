e44527e12709bfed986a10847e51ef1b695df472
Hosein <gasterhosein@gmail.com>
2024-08-29 20:32:20 +0330

    add number to alphabet and days challenge

---
+++ Number to Alphabet and Number to Days of the week/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Number_to_Alphabet_and_Number_to_Days_of_the_week
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
