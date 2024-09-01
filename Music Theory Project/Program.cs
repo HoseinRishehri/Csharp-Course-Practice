0749d7d564e3f71e3e8670088de26660fea6e6d7
Hosein <gasterhosein@gmail.com>
2024-09-01 20:21:49 +0330

    delethe and add code to music project

--- Music Theory Project/Program.cs
+++ Music Theory Project/Program.cs
@@ -6,7 +6,131 @@ namespace Music_Theory_Project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+             string userAnswer = null;
+
+
+            while (userAnswer != "bb")
+            {
+                Console.Clear();
+                Console.WriteLine($"Hi, this is Music Theory Practice Project!");
+                Console.WriteLine($"1. Give you random notes and you should write it on sheet");
+                Console.WriteLine($"");
+                Console.WriteLine($"Wite 'bb' to quiet the project");
+                Console.WriteLine($"");
+                Console.Write("--->   ");
+                userAnswer = Console.ReadLine();
+
+
+                switch (userAnswer)
+                {
+                    // practice number one, random note!
+                    case "1":
+                        Console.Clear();
+                        string practiceOneUserAnswer = null;
+
+                        Console.WriteLine($"Here's the Tutorial:");
+                        Console.WriteLine($"I write a note with 1.Name 2.Octave 3.Rhtyem every time you press the Enter!");
+                        Console.WriteLine($"For example:  5B w means B note on third line with whole-note rythem.");
+                        Console.WriteLine($"Press enter to start or press any other key for exit.");
+                        Console.WriteLine($"");
+                        Console.Write($"--->  ");
+                        practiceOneUserAnswer = Console.ReadLine();
+
+                        while (practiceOneUserAnswer == "")
+                        {
+
+                            Console.Clear();
+                            string name = null;
+                            int octave;
+                            string rythem = null;
+                            Random random = new Random();
+
+                            var randomNumber1 = random.Next(1, 8);
+                            var randomNumber2 = random.Next(1, 3);
+                            var randomNumber3 = random.Next(1, 7);
+
+                            switch (randomNumber1)
+                            {
+                                case 1:
+                                    name = "C";
+                                    break;
+                                case 2:
+                                    name = "D";
+                                    break;
+                                case 3:
+                                    name = "E";
+                                    break;
+                                case 4:
+                                    name = "F";
+                                    break;
+                                case 5:
+                                    name = "G";
+                                    break;
+                                case 6:
+                                    name = "A";
+                                    break;
+                                case 7:
+                                    name = "B";
+                                    break;
+                                default:
+                                    name = "wtf!";
+                                    break;
+                            }
+
+                            switch (randomNumber2)
+                            {
+                                case 1:
+                                    octave = 4;
+                                    break;
+                                case 2:
+                                    octave = 5;
+                                    break;
+                                default:
+                                    octave = 99999;
+                                    break;
+                            }
+
+                            switch (randomNumber3)
+                            {
+                                case 1:
+                                    rythem = "w";
+                                    break;
+                                case 2:
+                                    rythem = "h";
+                                    break;
+                                case 3:
+                                    rythem = "q";
+                                    break;
+                                case 4:
+                                    rythem = "e";
+                                    break;
+                                case 5:
+                                    rythem = "s";
+                                    break;
+                                case 6:
+                                    rythem = "t";
+                                    break;
+                                default:
+                                    rythem = "wtf!";
+                                    break;
+                            }
+
+                            Console.WriteLine($"w = whole-note / h = half-note / q = quarter-note / e = eight-note / s = sixtheentnote / t = thirtytwonote");
+                            Console.WriteLine($"");
+                            Console.WriteLine($"");
+                            Console.WriteLine($"Your random note is:  {name + octave} {rythem}");
+                            practiceOneUserAnswer = Console.ReadLine();
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+
+            Console.Clear();
+            Console.WriteLine($"Made by H.g");
+
         }
     }
 }
